070e7e898f338567611d0bd1b1961d7ec8efc5a4
GregJuge <gregoryjuge5@gmail.com>
2020-10-07 11:24:31 +0200

    va mourir Git

---
+++ script_formulaire.js
@@ -0,0 +1,7 @@
+document.querySelector("#button").addEventListener('click', function(event) {
+    event.preventDefault();
+    alert("Merci de votre intérêt, nous vous contacterons dans les meilleurs délais");
+  });
+
+
+
