f6a562e8af2f9630dbf482df362e0fc363d63620
J <jonathan.arnould33@gmail.com>
2021-01-07 22:30:07 +0100

    fix form

--- script_formulaire.js
+++ script_formulaire.js
@@ -1,7 +1,6 @@
-document.querySelector("#button").addEventListener('click', function(event) {
-    event.preventDefault();
-    alert("Merci de votre intérêt, nous vous contacterons dans les meilleurs délais");
-  });
-
-
-
+document.querySelector("#button").addEventListener("click", function (event) {
+  event.preventDefault();
+  alert(
+    "Merci de votre intérêt, nous vous contacterons dans les meilleurs délais!"
+  );
+});
